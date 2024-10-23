6b886f311320b4884b80dfd6a8ebbabfbe6e139f
Moutee Al-Sabouni <Moutee.sab@gmail.com>
2024-10-23 20:56:47 +0300

    Add Filament StudentResource

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Section;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->autofocus()->required(),
+                TextInput::make('email')->required()->unique(),
+                Select::make('class_id')->relationship('class', 'name')->live()->required(),
+                Select::make('section_id')
+                    ->options(function (Forms\Get $get) {
+                        $classId = $get('class_id');
+
+                        if($classId) return Section::where('class_id', $classId)->pluck('name', 'id');
+                    })
+                    ->required(),
+                TextInput::make('password')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
+                TextColumn::make('class.name')->badge()->sortable(),
+                TextColumn::make('section.name')->badge(),
+                TextColumn::make('created_at')->label('Join Date')->sortable(),
+            ])
+            ->persistSortInSession()
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
