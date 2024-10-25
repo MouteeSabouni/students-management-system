27371b4a7d2b3cda40a18e52b6e8da9d2fc99350
Moutee Al-Sabouni <Moutee.sab@gmail.com>
2024-10-25 22:55:14 +0300

    Add tabs to ClassesResource list

--- app/Filament/Resources/ClassesResource/Pages/ListClasses.php
+++ app/Filament/Resources/ClassesResource/Pages/ListClasses.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ClassesResource\Pages;
 
 use App\Filament\Resources\ClassesResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListClasses extends ListRecords
 {
@@ -16,4 +18,16 @@ class ListClasses extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All classes'),
+            'active' => Tab::make('Classes with no sections')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDoesntHave('sections')),
+            'inactive' => Tab::make('Classes with no students')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDoesntHave('students')),
+        ];
+    }
+
 }
